a85c19c21e5a868afc9ba656b2a40e21aae50e68
Your Name <you@example.com>
2026-08-31 01:13:22 +0530

    final

--- target/compiled/dbt_pg_tutorial/models/marts/dim_customers.sql
+++
@@ -1,9 +0,0 @@
-
-
-select
-    c.customer_id,
-    initcap(c.first_name) as first_name,
-    initcap(c.last_name)  as last_name,
-    c.email,
-    c.created_at
-from "dbt_tutorial"."analytics_staging"."stg_customers" c
